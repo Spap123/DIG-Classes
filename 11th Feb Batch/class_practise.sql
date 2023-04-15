65068b310a7c75a7330769883121e1e579e9bfef
Chandan Sengupta <Csengupta1101@outlook.com>
2023-04-15 11:28:05 +0530

    Class Commit

--- 11th Feb Batch/class_practise.sql
+++ 11th Feb Batch/class_practise.sql
@@ -1,8 +1,3 @@
-SELECT e.first_name , e.last_name , b.branch_name,
-CASE 
-    WHEN salary>70000 THEN salary+(salary*0.2)
-    WHEN salary BETWEEN 60000 and 70000 then salary+(salary*0.3)
-    ELSE salary+(salary*0.6)
-END AS emp_incremented_salary
-FROM employee e JOIN branch b
-ON e.branch_id = b.branch_id;
+select g.*,
+nth_value(name,2) over(partition by category order by price desc range between unbounded preceding 
+and unbounded following) from gadgets g;
