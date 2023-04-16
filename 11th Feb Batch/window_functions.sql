98c1d65c78ee4ea3d4806c8e869327235a733e89
Chandan Sengupta <Csengupta1101@outlook.com>
2023-04-16 10:47:55 +0530

    ML Commit

--- 11th Feb Batch/window_functions.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.* ,
-ROW_NUMBER() OVER(PARTITION BY emp_department ORDER BY emp_salary) AS Rownum ,
-RANK() OVER(PARTITION BY emp_department ORDER BY emp_salary) AS Rnk,
-DENSE_RANK() OVER(PARTITION BY emp_department ORDER BY emp_salary) AS DRnk FROM
-details e;
